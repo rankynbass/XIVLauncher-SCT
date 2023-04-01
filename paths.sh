a0844a0d5f8150e6baa5b1f881c4cac19dceea3c
Rankyn Bass <rankyn@proton.me>
2023-04-01 15:56:32 -0700

    Add submodule

---
+++ paths.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export src=/tmp/xlcore-src
+export bin=/tmp/xlcore-bin
+export xlcore=$HOME/projects/XIVLauncher.Core
+export ffql=$HOME/projects/FFXIVQuickLauncher
+export ver=1.0.3.0
+export hash=Steam-Compatibility-Tool
+export coreRepo=https://github.com/rankynbass/XIVLauncher.Core.git
+export coreTag=steamcompattool
+export ffqlRepo=https://github.com/goatcorp/FFXIVQuickLauncher.git
+export ffqlTag=steamcompattool
+# use -clp:ErrorsOnly to turn off warnings.
+export noWarnings=-clp:ErrorsOnly
