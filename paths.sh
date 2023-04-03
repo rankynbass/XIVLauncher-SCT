05c41ba6d0aee0f95c6224fdce32bcea225d4fcf
Rankyn Bass <rankyn@proton.me>
2023-04-02 20:45:02 -0700

    Update versioning to be separate from XIVLauncher.Core

--- paths.sh
+++ paths.sh
@@ -6,8 +6,8 @@ export xlcore=$HOME/projects/XIVLauncher.Core
 export ffql=$HOME/projects/FFXIVQuickLauncher
 # use -clp:ErrorsOnly to turn off warnings. Comment out to enable warnings.
 export noWarnings=-clp:ErrorsOnly
-# Name of output file
-export ver=1.0.3
-export rel=1
-export hash=r$rel-SCT
-export outname=XIVLauncher-SCT-$ver-r$rel.tar.gz
+# Name of output file.
+export ver=0.1.0
+export rel=alpha.1
+export hash=$rel
+export outname=XIVLauncher-SCT-$ver-$rel.tar.gz
