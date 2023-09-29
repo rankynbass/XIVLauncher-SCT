360b348ac6f42893d19e643f167929bd6872cd7d
Rankyn Bass <rankyn@proton.me>
2023-09-28 21:46:14 -0700

    Update to 0.3.0

--- paths.sh
+++ paths.sh
@@ -7,7 +7,7 @@ export ffql=$HOME/projects/FFXIVQuickLauncher
 # use -clp:ErrorsOnly to turn off warnings. Comment out to enable warnings.
 export noWarnings=-clp:ErrorsOnly
 # Name of output file.
-export ver=0.2.0
-export rel=alpha
+export ver=0.3.0
+export rel=beta
 export hash=$rel
 export outname=XIVLauncher-SCT-$ver-$rel.tar.gz
